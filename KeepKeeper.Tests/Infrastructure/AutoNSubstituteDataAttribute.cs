ce10e52c626f338ef24cec9802eea34cedc471c9
SergeyVolodko <volodkosm@gmail.com>
2016-08-12 13:30:08 +0200

    Packeges for test project are installed

---
+++ KeepKeeper.Tests/Infrastructure/AutoNSubstituteDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Xunit2;
+
+namespace KeepKeeper.Tests.Infrastructure
+{
+    public class AutoNSubstituteDataAttribute : AutoDataAttribute
+    {
+        public AutoNSubstituteDataAttribute()
+            : base(new Fixture().Customize(new AutoNSubstituteCustomization()))
+        {
+        }
+    }
+}
---
+++ KeepKeeper.Tests/Infrastructure/ControllerAutoDataAttribute.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Kernel;
+using Ploeh.AutoFixture.Xunit2;
+
+namespace KeepKeeper.Tests.Infrastructure
+{
+    public class ControllerAutoDataAttribute : AutoDataAttribute
+    {
+        public ControllerAutoDataAttribute()
+            : base(new Fixture().Customize(new ControllerCustomization()))
+        {
+        }
+    }
+
+    internal class ControllerCustomization : CompositeCustomization
+    {
+        internal ControllerCustomization()
+            : base(
+                new HttpRequestMessageCustomization(),
+                new ApiControllerCustomization(),
+                new AutoNSubstituteCustomization()
+                //new IgnoreVirtualMembersCustomization()
+                )
+        { }
+    }
+
+    internal class HttpRequestMessageCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<HttpRequestMessage>(c => c
+                .Without(x => x.Content)
+                .Do(x => x.Properties[HttpPropertyKeys.HttpConfigurationKey] =
+                    new HttpConfiguration()));
+        }
+    }
+
+    internal class ApiControllerCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customizations.Add(
+                new FilteringSpecimenBuilder(
+                    new Postprocessor(
+                        new MethodInvoker(
+                            new ModestConstructorQuery()
+                        ),
+                        new ApiControllerFiller()
+                    ),
+                    new ApiControllerSpecification()
+                ));
+        }
+
+        private class ApiControllerFiller : ISpecimenCommand
+        {
+            public void Execute(object specimen, ISpecimenContext context)
+            {
+                if (specimen == null)
+                    throw new ArgumentNullException("specimen");
+                if (context == null)
+                    throw new ArgumentNullException("context");
+
+                var target = specimen as ApiController;
+                if (target == null)
+                    throw new ArgumentException(
+                        "The specimen must be an instance of ApiController.",
+                        "specimen");
+
+                target.Request =
+                    (HttpRequestMessage)context.Resolve(
+                        typeof(HttpRequestMessage));
+            }
+        }
+
+        private class ApiControllerSpecification : IRequestSpecification
+        {
+            public bool IsSatisfiedBy(object request)
+            {
+                var requestType = request as Type;
+                if (requestType == null)
+                    return false;
+                return typeof(ApiController).IsAssignableFrom(requestType);
+            }
+        }
+    }
+}
